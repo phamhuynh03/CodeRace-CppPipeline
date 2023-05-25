9af677c8f526ca6a2f141dab0d7010fc15e278d8
Ha The Viet <aiv1hc@bosch.com>
2023-05-25 18:10:06 +0700

    Add function that is accidentially deleted

--- src/track2/code_with_bugs.c
+++ src/track2/code_with_bugs.c
@@ -136,3 +136,27 @@ int calculate_fingerprint(unsigned long uid, unsigned long counter, const char *
 
     return 0;
 }
+
+void exploreMe(int a, int b, string c)
+{
+    printf("a: %d; b: %d; c: %s\n", a, b, c.c_str());
+
+    if (a >= 20000)
+    {
+        if (b >= 2000000)
+        {
+            if (b - a < 100000)
+            {
+                // Trigger the undefined behavior sanitizer
+                int n = 23;
+                n <<= 32;
+                if (c == "FUZZING")
+                {
+                    // Trigger a heap buffer overflow
+                    char *s = (char *)malloc(1);
+                    strcpy(s, "too long");
+                }
+            }
+        }
+    }
+}
