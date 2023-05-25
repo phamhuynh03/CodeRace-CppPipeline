4a260b91c99c75d604ba3dc75220bc9e6906c74e
Trịnh Hoàng Anh <113800514+thammgh37@users.noreply.github.com>
2023-05-25 21:23:40 +0700

    Update crc32.c

--- src/track2/crc32.c
+++ src/track2/crc32.c
@@ -1,11 +1,11 @@
-unsigned int crc32(unsigned char *message) {
+unsigned int crc32(const unsigned char *message) {
    int i, j;
-   unsigned int byte, crc, mask;
+   unsigned int  crc, mask;
 
    i = 0;
    crc = 0xFFFFFFFF;
    while (message[i] != 0) {
-      byte = message[i];            // Get next byte.
+      unsigned int byte = message[i];            // Get next byte.
       crc = crc ^ byte;
       for (j = 7; j >= 0; j--) {    // Do eight times.
          mask = -(crc & 1);
