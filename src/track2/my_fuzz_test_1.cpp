52510d90817831b2e5366f3b239614f2146756f1
Marclevy <anh.nguyence0912@hcmut.edu.vn>
2023-05-26 09:41:26 +0700

    fuzz file test

--- src/track2/my_fuzz_test_1.cpp
+++ src/track2/my_fuzz_test_1.cpp
@@ -1,28 +1,18 @@
 #include <assert.h>
-
+#include "code_with_bugs.cpp"
 #include <cifuzz/cifuzz.h>
 #include <fuzzer/FuzzedDataProvider.h>
 
-FUZZ_TEST_SETUP() {
+FUZZ_TEST_SETUP()
+{
   // Perform any one-time setup required by the FUZZ_TEST function.
 }
 
-FUZZ_TEST(const uint8_t *data, size_t size) {
-  // FuzzedDataProvider provides convenience methods that turn the raw fuzzer
-  // data into common types. Use it to generate parameters for the function you
-  // want to fuzz:
-  //
-  // FuzzedDataProvider fuzzed_data(data, size);
-  // int my_int = fuzzed_data.ConsumeIntegral<int8_t>();
-  // std::string my_string = fuzzed_data.ConsumeRandomLengthString();
-
-  // Call the functions you want to test with the provided data and optionally
-  // assert that the results are as expected:
-  //
-  // int res = DoSomething(my_int, my_string);
-  // assert(res != -1);
-
-  // If you want to know more about writing fuzz tests you can check out the
-  // example projects at https://github.com/CodeIntelligenceTesting/cifuzz/tree/main/examples
-  // or have a look at our docs at https://docs.code-intelligence.com/
+FUZZ_TEST(const uint8_t *data, size_t size)
+{
+  FuzzDataProvider fuzzed_data(data, size);
+  int a = fuzzed_data.ConsumeIntegral();
+  int b = fuzzed_data.ConsumeIntegral();
+  std::string c = fuzzed_data.ConsumeRandomLengthString();
+  exploreMe(a, b, c);
 }
